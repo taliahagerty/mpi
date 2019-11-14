d53d121093727a113ca7e38407068bb107cfc859
taliahagerty <thagerty@economicsandpeace.org>
2019-11-15 10:53:29 +1100

    First Commit

---
+++ R/state.code.R
@@ -0,0 +1,23 @@
+# a funciton to fill in state codes
+# dictionary is cached from ./munge/00-state name dictionary.R
+
+state.code <- function(x, drop.state = T) {
+  
+  x$state <- tolower(x$state)
+  dictionary = mpifunctions::dictionary
+  dictionary$state <- tolower(dictionary$state)
+  
+  key = match(x$state, dictionary$state)
+  
+  x$geocode = dictionary$code[key]
+  
+  # pos = which(is.na(key))
+  
+  # x$code[pos]
+  
+  
+  if(drop.state == T) {
+    x <- x %>% select(-state)
+  }
+  
+  return(x) }
